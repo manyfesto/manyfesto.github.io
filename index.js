f796d3f2242ea5d73dc7cbc99919f5e6e8fa051c
tharcissie <tharcissieidufashe@gmail.com>
2021-09-22 09:33:59 +0200

    shuffling names

---
+++ index.js
@@ -0,0 +1,21 @@
+let names = ['R. Buse Çetin', 'Jessica Fjeld', 'Malavika Jayaram', 'Paola Ricaurte Quijano', 'John Havens', 'Sabelo Mhlambi']
+document.getElementById("names").onload = function () { shuffle() };
+
+function shuffle(array) {
+    var currentIndex = array.length, randomIndex;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+            array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
+shuffle(names)
+document.getElementById("names").innerHTML = names;
+
+
